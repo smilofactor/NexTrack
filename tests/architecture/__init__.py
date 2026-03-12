ee4ea62076d8d7f4eee79df975821570a3561e75
Steve M <5023125+smilofactor@users.noreply.github.com>
2026-03-11 22:45:15 -0400

    arch: Added tests/architecture to enforce NexTrack structure rules.

---
+++ tests/architecture/__init__.py
@@ -0,0 +1,7 @@
+"""
+This directory will contain tests to enforce project architecture.
+Starting with ensuring that domain does not import from other packages
+in NexTrack.  
+Domain can only import python or external packages, never from packages like
+services, infrastructure, tests
+"""
